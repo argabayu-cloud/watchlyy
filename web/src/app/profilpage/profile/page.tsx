62a90463b4ef36d3c90fa90a90fa5f3b93f63a55
Arga Bayu <argabayu-cloud@users.noreply.github.com>
2025-11-26 23:50:56 +0700

    memambahkan /profile/page.tsx

---
+++ web/src/app/profilpage/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function ProfilePage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-semibold mb-3">Profil</h1>
+      <p>Halaman untuk mengelola data profil.</p>
+    </div>
+  );
+}
