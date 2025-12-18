67fd495bc99f35b2b7c6c5de0dc097bfa10aa218
Arga Bayu <argabayu-cloud@users.noreply.github.com>
2025-12-18 13:43:01 +0700

    perbaharui fitur

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -64,8 +64,9 @@ Masukkan email dan password kamu </CardDescription> </CardHeader>
         <Button
           type="submit"
           className="w-full bg-red-600 hover:bg-red-700"
-        >
+        ><Link href="/film"> 
           Login
+          </Link>
         </Button>
       </form>
     </CardContent>
