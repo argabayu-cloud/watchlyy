f7447a6e27f5fe1d5f1d2b2e5cb66d537aa1642b
Erwingtw11 <erwinmyboy03@gmail.com>
2025-12-18 19:43:42 +0700

    refactor server.ts for improved route organization and readability

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,31 +1,35 @@
-  import express from "express";
-  import cors from "cors";
-  import { PrismaClient } from "@prisma/client";
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
-  // Routes
-  import genreRoutes from "./routes/genreRoute";
-  import authRoutes from "./routes/authRoutes";
-  import userRoutes from "./routes/userRoute";
-  import filmRoutes from "./routes/filmRoute";
+// Routes
+import genreRoutes from "./routes/genreRoute";
+import authRoutes from "./routes/authRoutes";
+import userRoutes from "./routes/userRoute";
+import filmRoutes from "./routes/filmRoute";
+import profilRoute from "./routes/profilRoute";
 
-  const app = express();
-  const prisma = new PrismaClient();
+const app = express();
+const prisma = new PrismaClient();
 
-  app.use(cors());
-  app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-  // Default root (cek API)
-  app.get("/", (req, res) => {
-    res.json({ message: "API Running..." });
-  });
+app.use("/api/auth", authRoutes);
 
-  // Register all routes
-  app.use("/api/genres", genreRoutes);
-  app.use("/api/auth", authRoutes);
-  app.use("/api/users", userRoutes);
-  app.use("/api/films", filmRoutes);
+// Default root (cek API)
+app.get("/", (req, res) => {
+  res.json({ message: "API Running..." });
+});
 
-  // Server
-  app.listen(3001, () => {
-    console.log("Server running at http://localhost:3001");
-  });
+// Register all routes
+app.use("/api/genres", genreRoutes);
+
+app.use("/api/users", userRoutes);
+app.use("/api/films", filmRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api", profilRoute);
+// Server
+app.listen(3001, () => {
+  console.log("Server running at http://localhost:3001");
+});
