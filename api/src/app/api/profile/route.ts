b1908b43cf0680d560173b97bbcf83f613e27901
Arga Bayu <argabayu-cloud@users.noreply.github.com>
2025-12-28 19:43:20 +0700

    buat fitur profil

---
+++ api/src/app/api/profile/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+let profile = {
+  username: "Current Username",
+  photo: "/images/WatchLy.png",
+};
+
+export async function GET() {
+  return NextResponse.json(profile);
+}
+
+export async function PUT(req: Request) {
+  const body = await req.json();
+
+  profile = {
+    ...profile,
+    ...body,
+  };
+
+  return NextResponse.json({
+    success: true,
+    profile,
+  });
+}
