1bafe96438035f50738c54b367f1632c35db00ec
Dani Suhandi <danisuhandi3@gmail.com>
2026-01-05 22:05:53 +0700

    hapus yg tak perlu

--- api/src/server.ts
+++ api/src/server.ts
@@ -15,20 +15,18 @@ const prisma = new PrismaClient();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-
-// Default root (cek API)
+// Root API
 app.get("/", (req, res) => {
   res.json({ message: "API Running..." });
 });
 
-// Register all routes
+// Register routes
+app.use("/api/auth", authRoutes);
 app.use("/api/genres", genreRoutes);
-
 app.use("/api/users", userRoutes);
 app.use("/api/films", filmRoutes);
-app.use("/api/auth", authRoutes);
 app.use("/api", profilRoute);
+
 // Server
 app.listen(3001, () => {
   console.log("Server running at http://localhost:3001");
