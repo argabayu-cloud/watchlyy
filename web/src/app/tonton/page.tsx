45264f246dfd39b00408be396cce39cc5aea7ec8
Dani Suhandi <danisuhandi3@gmail.com>
2026-01-14 23:17:31 +0700

    Menambahkan gambar poster THE RAID 3: GAJI BUTA

--- web/src/app/tonton/page.tsx
+++ web/src/app/tonton/page.tsx
@@ -208,7 +208,7 @@ export const actionMovies: Movie[] = [
     id: 26,
     title: "THE RAID 3: GAJI BUTA",
     description: "Kisah berlanjut dengan pertarungan brutal di dunia kriminal yang semakin gelap dan korup. Seorang mantan aparat harus kembali terjun ke dalam jaringan kejahatan demi membongkar pengkhianatan dan ketidakadilan yang merajalela. Aksi bela diri khas Indonesia ditampilkan secara intens, cepat, dan tanpa ampun. Film ini menawarkan koreografi laga yang realistis, emosi yang keras, serta kritik sosial yang tajam.",
-    poster: "/images/movie/the shadow.jpg",
+    poster: "/images/movie/action/26.jpg",
     genre: "action",
   },
   {
